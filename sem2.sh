b9b5ec0250624909b91eee5d650af9c028f35c30
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-08-07 05:53:38 -0500

    Add files via upload

---
+++ sem2.sh
@@ -0,0 +1,144 @@
+clear
+#colors
+#colors
+g='\033[1;32m'
+gg='\033[5;32m'
+ggg='\033[7;32m'
+gggg='\033[0;32m'
+
+p='\033[1;35m'
+pp='\033[5;35m'
+ppp='\033[7;35m'
+pppp='\033[0;35m'
+
+y='\033[1;33m'
+yy='\033[5;33m'
+yyy='\033[7;33m'
+yyyy='\033[0;33m'
+
+r='\033[1;31m'
+rr='\033[5;31m'
+rrr='\033[7;31m'
+rrrr='\033[0;31m'
+
+b='\033[1;34m'
+bb='\033[5;34m'
+bbb='\033[7;34m'
+bbbb='\033[0;34m'
+
+re='\033[0m'
+
+c='\033[1;36m'
+cc='\033[5;36m'
+ccc='\033[7;36m'
+cccc='\033[0;36m'
+
+
+
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+
+
+cd
+
+
+
+
+
+
+
+cd $HOME/payload5/.tool2/sem22
+bash tool.sh
+
+cd $HOME/payload5
+rm index.html
+wget ifconfig.me
+clear
+chat(){
+echo 'def kk(t):' > .sssss.py
+echo '   import sys, time' >> .sssss.py
+echo '   for txt in t + "\n":' >> .sssss.py
+echo '        sys.stdout.write(txt)' >> .sssss.py
+echo '        sys.stdout.flush()' >> .sssss.py
+echo '        time.sleep(9. / 240)' >> .sssss.py
+
+
+echo 'h = "              \033[1;33m[\033[1;32m*\033[1;33m] you welcome in {\033[1;36mpauload7\033[1;33m} py [\033[1;32mali.max\033[1;33m] "' >> .sssss.py
+echo 'kk(h)' >> .sssss.py
+python2 .sssss.py
+
+}
+chat2(){
+echo 'def kk(t):' > .ssss.py
+echo '   import sys, time' >> .ssss.py
+echo '   for txt in t + "\n":' >> .ssss.py
+echo '        sys.stdout.write(txt)' >> .ssss.py
+echo '        sys.stdout.flush()' >> .ssss.py
+echo '        time.sleep(4. / 240)' >> .ssss.py
+
+
+
+echo 'f = "    \033[1;32m [\033[1;33m30\033[1;32m] Continue my account {\033[1;34mFacebook\033[1;32m}"' >> .ssss.py
+echo 'y = "     \033[1;32m[\033[1;33m40\033[1;32m] Follow my channel {\033[1;31mYouTube\033[1;32m}"' >> .ssss.py
+echo 'kk(f)' >> .ssss.py
+echo 'kk(y)' >> .ssss.py
+python2 .ssss.py
+
+}
+echo -e "$g"
+figlet  -f big "           payload 7  "
+chat
+echo -e "$blue       .::::::::::.                           .::::::::::."
+sleep 0.1
+echo -e "     .::::''''''::::.$cyan Welcome to----->{2019}$blue .::::''''''::::."
+sleep 0.1
+echo -e "   .:::            ::::....          ....::::             :::."
+sleep 0.1
+echo -e "  .::'             ':::::::|        |:::::::'              '::'"
+sleep 0.1
+echo -e " .::                |::::::|_ ___ __|::::::|                ::."
+sleep 0.1
+echo -e " '--' $g   payload  $blue  |::::::|_()__()_|::::::|       $g 7 $blue      '--"
+sleep 0.1
+echo -e "  :::               |::-o::|        |::o-::|                :::"
+sleep 0.1
+echo -e "  '::.             .|::::::|        |::::::|.              .::'"
+sleep 0.1
+echo -e "   ':::.          .::\-----'        '-----/::.          .:::'"
+sleep 0.1
+echo -e "    '::::......::::'    $cyan   [90]helpℹ️  $blue      '::::......::::'"
+sleep 0.1
+echo -e "       '::::::::::'                           '::::::::::'     "
+sleep 0.1
+echo -e "                      $bluo p $green a $reset y $purple l $cyan o $yellow a $green d"
+echo -e "       $ggg+__________________________________________________+$gggg"
+echo -e "      $g $ggg |$gggg $p [$y 1$p ]$c root     $p [$y 2$p ]$c wifi     $p [$y 3$p ]$c spammer $ggg |$gggg"
+echo -e "      $g $ggg |$gggg $p [$y 4$p ]$c facebook $p [$y 5$p ]$c liunx    $p [$y 6$p ]$c camera  $ggg |$gggg"
+echo -e "      $g $ggg |$gggg $p [$y 7$p ]$c virus    $p [$y 8$p ]$c whatsapp $p [$y 9$p ]$c hackers $ggg |$gggg"
+echo -e "      $g $ggg |                 $gggg $p[$y 10$p ]$c ask $ggg                    |$gggg"
+echo -e "      $g       $ggg --------------------------------------- $gggg"
+echo -e "                $ccc --------------------------------- $cccc"
+echo -e "                   $ppp --------------------------- $pppp"
+echo -e "                      $yyy --------------------- $yyyy"
+
+
+echo -e " $g[$c 00$g ]$r Exit                                      $g[$c 000$g ]$y Back"
+
+
+echo -e "$cyan"
+echo -e "|---{$yellow my ip$cyan }---| "
+cat index.html
+echo -e "$blue"
+ifconfig wlan0 | grep -o 192..........
+#echo -e "$green "
+
+
